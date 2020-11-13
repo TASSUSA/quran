09b7b67eef08ddc579fb1e7d9cdb5fe619b4ddb1
Andrew Wessels <shixish@gmail.com>
2020-11-12 20:51:23 -1000

    fix jquery reference

--- quran.js
+++ quran.js
@@ -1,7 +1,7 @@
-const initialize = function(){
-	var verseCounts = [7,286,200,176,120,165,206,75,129,109,123,111,43,52,99,128,111,110,98,135,112,78,118,64,77,227,93,88,69,60,34,30,73,54,45,83,182,88,75,85,54,53,89,59,37,35,38,29,18,45,60,49,62,55,78,96,29,22,24,13,14,11,11,18,12,12,30,52,52,44,28,28,20,56,40,31,50,40,46,42,29,19,36,25,22,17,19,26,30,20,15,21,11,8,8,19,5,8,8,11,11,8,3,9,5,4,7,3,6,3,5,4,5,6];
+const initialize = function($ = window.jQuery){
+	const verseCounts = [7,286,200,176,120,165,206,75,129,109,123,111,43,52,99,128,111,110,98,135,112,78,118,64,77,227,93,88,69,60,34,30,73,54,45,83,182,88,75,85,54,53,89,59,37,35,38,29,18,45,60,49,62,55,78,96,29,22,24,13,14,11,11,18,12,12,30,52,52,44,28,28,20,56,40,31,50,40,46,42,29,19,36,25,22,17,19,26,30,20,15,21,11,8,8,19,5,8,8,11,11,8,3,9,5,4,7,3,6,3,5,4,5,6];
 
-	var Suras = ["Al-Fatiha","Al-Baqara","Al-'Imran","An-Nisa'","Al-Ma'ida","Al-An'am","Al-A'raf","Al-Anfal","At-Tauba","Yunus","Hud","Yusuf","Ar-Ra'd","Ibrahim","Al-Hijr","An-Nahl","Al-Isra'","Al-Kahf","Maryam","Ta-Ha","Al-Anbiyaa","Al-Hajj","Al-Mu’minun","An-Nur","Al-Furqan","Ash-Shu’ara’","An-Naml","Al-Qasas","Al-Ankabut","Ar-Rum","Luqman","As-Sajda","Al-Ahzâb","Saba'","Fátir","Ya-Si(e’)n","As-Saffát","Sád","Az-Zumar","Gháfer or Al- Mu’min","Ha-Mim or Ha-Mim Sajda","Ash-Shu’ra","Az-Zukhruf","Ad-Dukhán","Al-Játhiya","Al-Ahqáf","Muhammad","Al-Fath","Al-Hujurat","Qáf","Adh-Dháriyat","At-Túr","An-Najm","Al-Qamar","Ar-Rahmán","Al-Wáqi’ah","Al-Hadid","Al-Mujadila","Al-Hashr","Al-Mumtahana","As-Saff","Al-Jumuah","Al-Munafiqún","At-Taghabún","At-Talaq","At-Tahrim","Al-Mulk","Al-Qalam","Al-Háqqa","Al-Ma’árij","Nuh","Al-Jinn","Al-Muzzammil","Al-Muddaththir","Al-Qiyámah","Ad-Dahr","Al-Mursalat","An-Naba’","An-Nazi'at","'Abasa","At-Takwir","Al-Infitár","Al-Mutaffifin","Al-Inshiqáq","Al-Burúj","At-Táriq","Al-Alá","Al-Gháshiya","Al-Fajr","Al-Balad","Ash-Shams","Al-Layl","Ad-Duhá","Ash-Sharh or Al- Inshiráh","At-Tin","Al-'Alaq","Al-Qadr","Al-Baiyina","Az-Zalzalah","Al-'Adiyát","Al-Qári'ah","At-Takáthur","Al-'Asr","Al-Humazah","Al-Fil","Quraish","Al-Má'ún","Al-Kawthar","Al-Káfirún","An-Nasr","Al-Masad","Al-Ikhlás","Al-Falaq","An-Nás"];
+	const Suras = ["Al-Fatiha","Al-Baqara","Al-'Imran","An-Nisa'","Al-Ma'ida","Al-An'am","Al-A'raf","Al-Anfal","At-Tauba","Yunus","Hud","Yusuf","Ar-Ra'd","Ibrahim","Al-Hijr","An-Nahl","Al-Isra'","Al-Kahf","Maryam","Ta-Ha","Al-Anbiyaa","Al-Hajj","Al-Mu’minun","An-Nur","Al-Furqan","Ash-Shu’ara’","An-Naml","Al-Qasas","Al-Ankabut","Ar-Rum","Luqman","As-Sajda","Al-Ahzâb","Saba'","Fátir","Ya-Si(e’)n","As-Saffát","Sád","Az-Zumar","Gháfer or Al- Mu’min","Ha-Mim or Ha-Mim Sajda","Ash-Shu’ra","Az-Zukhruf","Ad-Dukhán","Al-Játhiya","Al-Ahqáf","Muhammad","Al-Fath","Al-Hujurat","Qáf","Adh-Dháriyat","At-Túr","An-Najm","Al-Qamar","Ar-Rahmán","Al-Wáqi’ah","Al-Hadid","Al-Mujadila","Al-Hashr","Al-Mumtahana","As-Saff","Al-Jumuah","Al-Munafiqún","At-Taghabún","At-Talaq","At-Tahrim","Al-Mulk","Al-Qalam","Al-Háqqa","Al-Ma’árij","Nuh","Al-Jinn","Al-Muzzammil","Al-Muddaththir","Al-Qiyámah","Ad-Dahr","Al-Mursalat","An-Naba’","An-Nazi'at","'Abasa","At-Takwir","Al-Infitár","Al-Mutaffifin","Al-Inshiqáq","Al-Burúj","At-Táriq","Al-Alá","Al-Gháshiya","Al-Fajr","Al-Balad","Ash-Shams","Al-Layl","Ad-Duhá","Ash-Sharh or Al- Inshiráh","At-Tin","Al-'Alaq","Al-Qadr","Al-Baiyina","Az-Zalzalah","Al-'Adiyát","Al-Qári'ah","At-Takáthur","Al-'Asr","Al-Humazah","Al-Fil","Quraish","Al-Má'ún","Al-Kawthar","Al-Káfirún","An-Nasr","Al-Masad","Al-Ikhlás","Al-Falaq","An-Nás"];
 
 	$.fn.Quran = function(){
     var $quran = this;
@@ -289,6 +289,6 @@ const initialize = function(){
 };
 
 window.addEventListener('DOMContentLoaded', function(){
-  initialize(window.jQuery);
+  initialize();
   $('#quran').Quran();
 });
